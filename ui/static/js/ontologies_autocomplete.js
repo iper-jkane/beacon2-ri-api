3224200e67022f021eeba114284eb1b3330fcaca
Marta Ferri <marta.ferri.p@gmail.com>
2021-01-19 12:38:02 +0100

    Remember used ontologies and fix height bug

--- ui/static/js/ontologies_autocomplete.js
+++ ui/static/js/ontologies_autocomplete.js
@@ -3,8 +3,10 @@
     var $ontologiesFilter = $('#ontologies-filter');
     var $ontologiesFilterHidden = $("#filters-hidden");
     var $ontologiesTags = $("#filters-tags");
-
     var selectedOntologies = [];
+    if ($ontologiesFilterHidden.val()) {
+        var selectedOntologies = $ontologiesFilterHidden.val().split(",");
+    };
 
     $ontologiesFilter.autocomplete({
 	minLength: 1
@@ -23,11 +25,11 @@
             if (!$ontologiesTags.hasClass("active")) {
                 $ontologiesTags.addClass("active");
             };
-            if (selectedOntologies.indexOf(ui.item.ontology) == -1) {
+            if (selectedOntologies.indexOf(ui.item.label) == -1) {
                 // create tag
-                $ontologiesTags.append("<span data-ontology='"+ui.item.ontology+"' >"+ui.item.label+"<i class='fas fa-times'></i></span>")
+                $ontologiesTags.append("<span data-ontology='"+ui.item.label+"' >"+ui.item.label+"<i class='fas fa-times'></i></span>")
                 // add to list
-                selectedOntologies.push(ui.item.ontology);
+                selectedOntologies.push(ui.item.label);
                 // update hidden input
                 $ontologiesFilterHidden.val(selectedOntologies.join());
             };
